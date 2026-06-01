4c426316a629696750897a9ae50d597bd0f5d195
Keira Poland <252746079+keira-p@users.noreply.github.com>
2026-06-01 12:17:23 +0100

    Added tests for excluding games below similarity threshold

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -97,3 +97,42 @@ def test_recommender_multiple_game_excludes_all_selected_games():
     recommended_names = recommendations["Name"].values
     for game in selected_games:
         assert game not in recommended_names
+
+
+def test_recommender_exclude_games_below_similarity_threshold():
+    # Arrange
+    item_neighbours_df = pd.DataFrame(
+        {
+            "BGGId":[1, 1, 1],
+            "SimilarBGGId":[1, 2, 3],
+            "Score":[1.0, 0.9, 0.2]
+            }
+        )
+
+    name_to_id = {
+        "Catan": 1,
+        "Ticket to Ride": 2,
+        "Pandemic": 3,
+        }
+
+    id_to_name = {
+        1: "Catan",
+        2: "Ticket to Ride",
+        3: "Pandemic",
+        }
+
+    # Act
+    recommendations = recommend_from_favourite_games(
+        favourite_game_names=["Catan"],
+        item_neighbours_df=item_neighbours_df,
+        name_to_id=name_to_id,
+        id_to_name=id_to_name,
+        similarity_threshold=0.25,
+        top_n=20,
+        top_k_similar=50,
+        )
+
+    # Assert
+    recommended_names = recommendations["Name"].values
+    assert "Ticket to Ride" in recommended_names
+    assert "Pandemic" not in recommended_names
